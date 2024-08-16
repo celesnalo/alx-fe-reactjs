376e31f0311ff163e0fe83c58a135afc327750f5
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-08-16 03:52:15 +0200

    fixed color

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,6 @@
 function Header(){
     return(
-        <header style={{ background: "rgb(63,94,251)",
-            background: "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(173,70,252,1) 74%)",
+        <header style={{ backgroundColor: "rgb(63,94,251)",
             color: 'white', textAlign: 'center' , borderRadius: '10px'}}>
             <h1>My Favorite Cities</h1>
         </header>
