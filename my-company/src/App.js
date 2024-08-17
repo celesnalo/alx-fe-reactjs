e6040164b307b8ae7a9acbac1d0a8cfa3a08ddee
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-08-18 01:37:52 +0200

    company

---
+++ my-company/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
