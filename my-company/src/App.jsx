633db1c294a3cae5f4c20d80ea4f947cd45addae
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-08-18 01:51:05 +0200

    company website

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
