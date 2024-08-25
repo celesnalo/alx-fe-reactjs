5189db73bb09be997d9a9d416a0297f15502942c
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-08-25 20:04:45 +0200

    zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import AddRecipeForm from './components/AddReciprForm'
+import RecipeList from './components/RecipeList'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
