5fb61e21aa191cb180ddc9d3ad776629f4fc78c8
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-08-25 21:46:46 +0200

    routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,27 @@
-import AddRecipeForm from './components/AddReciprForm'
-import RecipeList from './components/RecipeList'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddRecipeForm from './components/AddRecipeForm'; // Fixed typo in file name
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <AddRecipeForm />
-      <RecipeList />
-    </>
-  )
+    <Router>
+      <div>
+        <Switch>
+          
+          <Route path="/" exact>
+            <AddRecipeForm />
+            <RecipeList />
+          </Route>
+          
+          <Route path="/recipe/:id" exact>
+            <RecipeDetails />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,27 @@
-// recipeStore.js
 import create from 'zustand';
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // add a new recipe
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+
+  // update an existing recipe
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  // delete a recipe by id
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
+
+  // set all recipes (if needed)
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
-
