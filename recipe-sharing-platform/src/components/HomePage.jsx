f1076dd0be9641630a9addc710eaf4b80f7b845d
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-09-14 15:05:06 +0200

    hover, media

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,13 +11,13 @@ const HomePage = () => {
     }, [])
 
     return(
-        <div className="bg-blue-200 w-16 h-24 p-4 text-black rounded-lg">
+        <div className="bg-blue-200 w-16 h-24 p-4 text-black rounded-lg sm:max-w-screen-md md:max-w-screen-lg overflow-hidden shadow-md hover:shadow-orange-600">
             {data.map(item => {
                 <div>
                     <h2>{item.id}</h2>
                     <h1>{item.title}</h1>
                     <p className="text-gray-800">{item.image}</p>
-                    <img src={item.image} alt="food"  className="rounded-lg"/>
+                    <img src={item.image} alt="food"  className="rounded-lg w-full h-36 sm:h-48 object-cover"/>
                 </div>
             })}
         </div>
