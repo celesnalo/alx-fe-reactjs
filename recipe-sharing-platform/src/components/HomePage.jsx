b9c3d215ec29a22dddcffe43b8df6019e68becfe
Mthobisi Snalo Cele <celersnalor0119@gmail.com>
2024-09-14 20:29:29 +0200

    routing

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
@@ -14,11 +15,13 @@ const HomePage = () => {
       <h1 className="text-4xl font-bold mb-6">Recipe Sharing Platform</h1>
       <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl">
-            <img src={recipe.image} alt={recipe.title} className="rounded-lg w-full h-36 object-cover" />
-            <h2 className="text-2xl font-semibold mt-2">{recipe.title}</h2>
-            <p className="text-gray-600">{recipe.summary}</p>
-          </div>
+            <Link key={item.id} to={`/recipe/${item.id}`}>
+                <div key={recipe.id} className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl">
+                    <img src={recipe.image} alt={recipe.title} className="rounded-lg w-full h-36 object-cover" />
+                    <h2 className="text-2xl font-semibold mt-2">{recipe.title}</h2>
+                    <p className="text-gray-600">{recipe.summary}</p>
+                </div>
+            </Link>
         ))}
       </div>
     </div>
